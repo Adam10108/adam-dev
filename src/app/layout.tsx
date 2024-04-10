d7626ec917f4e4edb874fd666e2f1f606e4e24f3
Adam10108 <adam_atlas1998@hotmail.com>
2024-04-10 18:22:38 +0700

    Update `metadata.title`

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'ADAM.DEV',
+  title: 'Adam Ajsaen',
   description: 'My personal website',
 }
 
