3d2564a32d90eab6fb2b7f24af9601b176f016bc
Adam10108 <adam_atlas1998@hotmail.com>
2024-04-11 17:19:41 +0700

    Add simple my avatar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,24 @@
 import { Card } from '@/components'
+import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="min-h-screen max-w-screen-lg mx-auto flex flex-col xl:flex-row gap-4 md:gap-8 p-4 xl:p-24">
-      <Card width="w-full xl:w-2/5">Card: Personal details</Card>
+    <main className="min-h-screen max-w-screen-xl mx-auto flex flex-col xl:flex-row gap-4 md:gap-8 p-4 xl:p-24">
+      <Card width="w-full xl:w-2/5">
+        <div className="flex flex-row xl:flex-col items-center gap-4 md:gap-8">
+          <div className="image-wrapper w-20 h-20 xl:w-40 xl:h-40">
+            <Image src="/avatar.png" alt="My avatar" fill={true} />
+          </div>
+
+          <div className="text-left xl:text-center">
+            <h3 className="mb-1 md:mb-3">Adam Ajsaen</h3>
+            <div className="px-2 py-1 ring-1 ring-gray-900/5 rounded-lg bg-gray-900/5  text-center">
+              <p className="sub-header-3">Software Engineer</p>
+            </div>
+          </div>
+        </div>
+      </Card>
+
       <Card>Card: About me/ Resume</Card>
     </main>
   )
