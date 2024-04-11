1d35b510640a78d45d66a2057eca27c0e992ab11
Adam10108 <adam_atlas1998@hotmail.com>
2024-04-11 16:58:31 +0700

    Setup `@fontsource/poppins`

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,13 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {},
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: 'Poppins, sans-serif',
+      },
+    },
+  },
   plugins: [],
 }
 export default config
