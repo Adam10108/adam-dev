46456a5bf11a5b8fa8aad3cf9f9986e6a52e3021
Adam10108 <adam_atlas1998@hotmail.com>
2024-04-18 12:57:43 +0700

    Wrap children with `NextIntlClientProvider`

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import { NextIntlClientProvider, useMessages } from 'next-intl'
 
 import { SupportedLocale } from '@/libs/i18n'
 
@@ -7,16 +8,24 @@ export const metadata: Metadata = {
   description: 'My personal website',
 }
 
+export interface LocaleLayout {
+  children: React.ReactNode
+  params: { locale: SupportedLocale }
+}
+
 export default function LocaleLayout({
   children,
   params: { locale },
-}: Readonly<{
-  children: React.ReactNode
-  params: { locale: SupportedLocale }
-}>) {
+}: LocaleLayout) {
+  const messages = useMessages()
+
   return (
     <html lang={locale} suppressHydrationWarning={true}>
-      <body>{children}</body>
+      <body>
+        <NextIntlClientProvider locale={locale} messages={messages}>
+          {children}
+        </NextIntlClientProvider>
+      </body>
     </html>
   )
 }
